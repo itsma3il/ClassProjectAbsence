b4541ea79d06109ddeff3e1fbee496853a0ec3db
anasfalah3 <anasfalah3@gmail.com>
2023-11-12 21:09:43 +0100

    completing profile stagiaire front end by (anas falah et oussama korkech)

---
+++ src/assets/js/calender.js
@@ -0,0 +1,101 @@
+const daysContainer = document.querySelector(".days");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const todayBtn = document.querySelector(".today");
+const month = document.querySelector(".month");
+
+const months = [
+  "Janvier",
+  "Février",
+  "Mars",
+  "Avril",
+  "Mai",
+  "Juin",
+  "Juillet",
+  "Août",
+  "Septembre",
+  "Octobre",
+  "Novembre",
+  "Décembre",
+];
+
+const days = ["Dim", "Lun", "Ma", "Mer", "Jeu", "Ven", "Sam"];
+
+const date = new Date();
+let currentMonth = date.getMonth();
+let currentYear = date.getFullYear();
+
+const renderCalendar = () => {
+  date.setDate(1);
+  const firstDay = new Date(currentYear, currentMonth, 1);
+  const lastDay = new Date(currentYear, currentMonth + 1, 0);
+  const lastDayIndex = lastDay.getDay();
+  const lastDayDate = lastDay.getDate();
+  const prevLastDay = new Date(currentYear, currentMonth, 0);
+  const prevLastDayDate = prevLastDay.getDate();
+  const nextDays = 7 - lastDayIndex - 1;
+
+  month.innerHTML = `${months[currentMonth]} ${currentYear}`;
+
+  let days = "";
+
+  for (let x = firstDay.getDay(); x > 0; x--) {
+    days += `<div class="day prev">${prevLastDayDate - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDayDate; i++) {
+    if (
+      i === new Date().getDate() &&
+      currentMonth === new Date().getMonth() &&
+      currentYear === new Date().getFullYear()
+    ) {
+      days += `<div class="day today">${i}</div>`;
+    } else {
+      days += `<div class="day">${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="day next">${j}</div>`;
+  }
+
+  daysContainer.innerHTML = days;
+  hideTodayBtn();
+};
+
+nextBtn.addEventListener("click", () => {
+  currentMonth++;
+  if (currentMonth > 11) {
+    currentMonth = 0;
+    currentYear++;
+  }
+  renderCalendar();
+});
+
+prevBtn.addEventListener("click", () => {
+  currentMonth--;
+  if (currentMonth < 0) {
+    currentMonth = 11;
+    currentYear--;
+  }
+  renderCalendar();
+});
+
+todayBtn.addEventListener("click", () => {
+  currentMonth = date.getMonth();
+  currentYear = date.getFullYear();
+  renderCalendar();
+});
+
+function hideTodayBtn() {
+  if (
+    currentMonth === new Date().getMonth() &&
+    currentYear === new Date().getFullYear()
+  ) {
+    todayBtn.style.display = "none";
+  } else {
+    todayBtn.style.display = "flex";
+  }
+}
+
+renderCalendar();

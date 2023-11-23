5065ced7bb52fa97f820dde9290271ed18fff8bb
anasfalah3 <anasfalah3@gmail.com>
2023-11-24 00:37:49 +0100

    fixing bug styling only the first avertissement

--- src/assets/js/listeAvertissement.js
+++ src/assets/js/listeAvertissement.js
@@ -1,19 +1,25 @@
 // adding a class depend on the avertissement type to add somme style from avertissement.css
-let avertissementText = document.getElementById("avertissementText");
-if (
-  avertissementText.innerText === "Mise en garde 1" ||
-  avertissementText.innerText === "Mise en garde 2" ||
-  avertissementText.innerText === "Avertissement 1" ||
-  avertissementText.innerText === "Avertissement 2"
-) {
-  avertissementText.classList.add("avertissement");
-} else if (
-  avertissementText.innerText === "Exclusion de 2 j (1)" ||
-  avertissementText.innerText === "Exclusion de 2 j (2)" ||
-  avertissementText.innerText === "Exclusion temporaire" ||
-  avertissementText.innerText === "Exclusion définitive"
-) {
-  avertissementText.classList.add("exclusion");
-} else if (avertissementText.innerText === "Blame") {
-  avertissementText.classList.add("blame");
-}
+let avertissementText = document.querySelectorAll(".avertissementText");
+avertissementText.forEach((avertissement) => {
+      if (
+        avertissement.innerText === "Mise en garde 1" ||
+        avertissement.innerText === "Mise en garde 2" ||
+        avertissement.innerText === "Avertissement 1" ||
+        avertissement.innerText === "Avertissement 2"
+      ) {
+        avertissement.classList.add("avertissement");
+      } else if (
+        avertissement.innerText === "Exclusion de 2 j (1)" ||
+        avertissement.innerText === "Exclusion de 2 j (2)" ||
+        avertissement.innerText === "Exclusion temporaire" ||
+        avertissement.innerText === "Exclusion définitive"
+      ) {
+        avertissement.classList.add("exclusion");
+      } else if (avertissement.innerText === "Blame") {
+        avertissement.classList.add("blame");
+      }
+});
+
+      
+
+

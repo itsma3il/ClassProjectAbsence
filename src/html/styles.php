7e35b2f8ecd8da8622f6f77a613117d9bdcc639b
itsma3il <smallgraphics77@gmail.com>
2023-12-11 15:23:15 +0100

    importing the font offline

--- src/html/styles.php
+++ src/html/styles.php
@@ -1,5 +1,5 @@
 <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon1.png" />
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200;300;400;500;600;700;800&display=swap">
+<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200;300;400;500;600;700;800&display=swap"> -->
 <link rel="stylesheet" href="../assets/css/styles.min.css" />
 <link rel="stylesheet" href="../assets/css/avertissement.css">
 <link rel="stylesheet" href="../assets/css/listeStagiaires.css">

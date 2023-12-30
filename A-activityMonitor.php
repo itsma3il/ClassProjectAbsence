d93c56b640b4c6e29faed5ee6d08480aa5eac10a
Anir <anirbouzit18@gmail.com>
2023-12-30 13:06:55 +0100

    Activity log and deleted items pages updated

--- A-activityMonitor.php
+++ A-activityMonitor.php
@@ -73,14 +73,61 @@ $activites = $stmt->fetchAll(PDO::FETCH_ASSOC);
           </li>
         </ul>
         <div class="card-body">
-          <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade active show" id="pills-account" role="tabpanel" aria-labelledby="pills-account-tab" tabindex="0">
-              mes Activités contenue
-            </div>
+              <!-- table -->
+              <div class="table-responsive table-container rounded border border-light shadow-sm" style="max-height:400px;overflow-y: scroll;">
+                <table class="table table-hover align-middle" id="dataTable">
+                  <thead class="bg-gray-2  text-left fixed-thead">
+                    <tr>
+                      <th class="min-width-150 py-3 px-4 font-weight-medium">
+                        Journal d'activités D'utilisateur
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php if (!empty($activites)) : ?>
+                      <?php foreach ($activites as $activite) : ?>
+                        <tr>
+                          <td class="border-bottom fw-bold py-3 px-4">
+                            <?php
+                            // remove the @ from the username
+                            $usernameParts = explode('@', $activite['Username']);
+                            $name = isset($usernameParts[0]) ? $usernameParts[0] : $activite['Username'];
+
+                            $stagiaireCin = $activite['StagiaireCin'];
+                            $StagiaireName = "SELECT nom, prenom FROM stagiaire WHERE cin = ?";
+                            $stmt = $pdo_conn->prepare($StagiaireName);
+                            $stmt->execute([$stagiaireCin]);
+                            $stagiaireInfo = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                            if (!$stagiaireInfo) {
+                              $DeletedStagiaireName = "SELECT nom, prenom FROM deletedstagiaire WHERE cin = ?";
+                              $stmt = $pdo_conn->prepare($DeletedStagiaireName);
+                              $stmt->execute([$stagiaireCin]);
+                              $deletedStagiaireInfo = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                              if ($deletedStagiaireInfo) {
+                                $fullName = $deletedStagiaireInfo['nom'] . ' ' . $deletedStagiaireInfo['prenom'];
+                                echo "<span class='text-dark'>- Vous avez <b>{$activite['Action']}</b> {$fullName} (CIN: {$stagiaireCin}) le " . date('Y-m-d', strtotime($activite['Timestamp'])) . " à " . date('H:i:s', strtotime($activite['Timestamp'])) . ".</span>";
+                              }
+                            } else {
+                              $fullName = $stagiaireInfo['nom'] . ' ' . $stagiaireInfo['prenom'];
+                              echo "<span class='text-dark'>- Vous avez <b> {$activite['Action']}</b> {$fullName} (CIN: {$stagiaireCin}) le " . date('Y-m-d', strtotime($activite['Timestamp'])) . " à " . date('H:i:s', strtotime($activite['Timestamp'])) . ".</span>";
+                            }
+                            ?>
+                          </td>
+                        </tr>
+                      <?php endforeach; ?>
+                    <?php else : ?>
+                      <tr>
+                        <td>No Activities Available.</td>
+                      </tr>
+                    <?php endif; ?>
+                  </tbody>
+                </table>
+              </div>
             <div class="tab-pane fade" id="pills-notifications" role="tabpanel" aria-labelledby="pills-notifications-tab" tabindex="0">
                 users activities contenue
             </div>
-          </div>
         </div>
       </div>
 
--- elementsSupprimes.php
+++ elementsSupprimes.php
@@ -73,11 +73,11 @@ $activites = $stmt->fetchAll(PDO::FETCH_ASSOC);
         </h1>
         <!-- Liste Avertissement Supprimée -->
         <div class="row m-0">
-          <div class="col-7 ">
+          <div>
             <div class="card-body">
               <!-- table -->
-              <div class="table-responsive rounded border border-light shadow-sm" style="height:360px;overflow-y: scroll;">
-                <table class="table table-hover">
+              <div class="table-responsive table-container rounded border border-light shadow-sm" style="max-height:200px;overflow-y: scroll;">
+                <table class="table table-hover align-middle" id="dataTable">
                   <thead class="bg-gray-2 text-left fixed-thead">
                     <tr>
                       <th class="min-width-220 py-3 px-4 font-weight-medium">
@@ -144,63 +144,6 @@ $activites = $stmt->fetchAll(PDO::FETCH_ASSOC);
               </div>
             </div>
           </div>
-          <!-- Historique (log) -->
-          <div class="col-5">
-            <div class="card-body">
-              <!-- table -->
-              <div class="table-responsive rounded border border-light shadow-sm" style="height:360px;overflow-y: scroll;">
-                <table class="table table-hover">
-                  <thead class="bg-gray-2  text-left fixed-thead">
-                    <tr>
-                      <th class="min-width-150 py-3 px-4 font-weight-medium">
-                        Journal d'activités D'utilisateur
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <?php if (!empty($activites)) : ?>
-                      <?php foreach ($activites as $activite) : ?>
-                        <tr>
-                          <td class="border-bottom fw-bold py-3 px-4">
-                            <?php
-                            // remove the @ from the username
-                            $usernameParts = explode('@', $activite['Username']);
-                            $name = isset($usernameParts[0]) ? $usernameParts[0] : $activite['Username'];
-
-                            $stagiaireCin = $activite['StagiaireCin'];
-                            $StagiaireName = "SELECT nom, prenom FROM stagiaire WHERE cin = ?";
-                            $stmt = $pdo_conn->prepare($StagiaireName);
-                            $stmt->execute([$stagiaireCin]);
-                            $stagiaireInfo = $stmt->fetch(PDO::FETCH_ASSOC);
-
-                            if (!$stagiaireInfo) {
-                              $DeletedStagiaireName = "SELECT nom, prenom FROM deletedstagiaire WHERE cin = ?";
-                              $stmt = $pdo_conn->prepare($DeletedStagiaireName);
-                              $stmt->execute([$stagiaireCin]);
-                              $deletedStagiaireInfo = $stmt->fetch(PDO::FETCH_ASSOC);
-
-                              if ($deletedStagiaireInfo) {
-                                $fullName = $deletedStagiaireInfo['nom'] . ' ' . $deletedStagiaireInfo['prenom'];
-                                echo "<span class='text-dark'>- {$name} a <b>{$activite['Action']}</b> {$fullName} (CIN: {$stagiaireCin}) le " . date('Y-m-d', strtotime($activite['Timestamp'])) . " à " . date('H:i:s', strtotime($activite['Timestamp'])) . ".</span>";
-                              }
-                            } else {
-                              $fullName = $stagiaireInfo['nom'] . ' ' . $stagiaireInfo['prenom'];
-                              echo "<span class='text-dark'>- {$name} a <b> {$activite['Action']}</b> {$fullName} (CIN: {$stagiaireCin}) le " . date('Y-m-d', strtotime($activite['Timestamp'])) . " à " . date('H:i:s', strtotime($activite['Timestamp'])) . ".</span>";
-                            }
-                            ?>
-                          </td>
-                        </tr>
-                      <?php endforeach; ?>
-                    <?php else : ?>
-                      <tr>
-                        <td>No Activities Available.</td>
-                      </tr>
-                    <?php endif; ?>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
         </div>
         <div class="d-grid gap-2 col-6 mx-auto rounded-pill my-3" style="background-color: #1e905b; ">
           <button class="btn text-light" id="openPopup" type="button" onclick="openPopup()">Ajouter Stagiaire</button>
@@ -226,8 +169,8 @@ $activites = $stmt->fetchAll(PDO::FETCH_ASSOC);
         </form>
         <div class="card-body">
           <!-- table -->
-          <div class="table-responsive table-container rounded border border-light shadow-sm">
-            <table class="table table-hover align-middle" id="dataTable">
+          <div class="table-responsive table-container rounded border border-light shadow-sm" style="max-height:400px;overflow-y: scroll;">
+                <table class="table table-hover align-middle" id="dataTable">
               <thead class="bg-gray-2  text-left fixed-thead">
                 <tr>
                   <th class="min-width-220 py-3 px-4 font-weight-medium">
